3bf5c62a532bf473b0d7da07e84fd0b59247a961
Ilyin Danny <ilyindanny@gmail.com>
2022-07-08 15:08:03 +0300

    temp

---
+++ files/iOS/code/003.py
@@ -0,0 +1,25 @@
+
+for i in range(100, -1, -20):
+    print(i)
+
+
+line = ""
+for i in range(5):
+    line = ""
+    for j in range(5):
+        line += "*"
+    print(line)
+
+
+for i in range(5):
+    print('*')
+
+
+text = 'abcdefghijklmnopqrstuvwxyz'
+print(len(text))
+
+print(text[len(text)-2:])
+gbb vbb
+
+
+print(text[-2:])
