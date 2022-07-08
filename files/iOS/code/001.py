6a3ffc37551c1a90a5ae96d5fecabcfdcd651b45
Danny <ilyindanny@gmail.com>
2022-07-08 15:57:58 +0300

    Update 001.py

--- files/iOS/code/001.py
+++ files/iOS/code/001.py
@@ -2,9 +2,9 @@ input_number = int(input('Enter number: '))
 number_to_letter = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 for i in range(2, 37):
-    x = input_number
-    result_number = ''
-    while x > 0:
-        result_number += number_to_letter[x % i]
-        x //= i
-    print(f'{input_number} в C/C с основанием {i} = {result_number[::-1]}')
+        x = input_number
+        result_number = ''
+        while x > 0:
+            result_number += number_to_letter[x % i]
+            x //= i
+        print('{0} в C/C с основанием {1} = {2}'.format(input_number, i, result_number[::-1]))
