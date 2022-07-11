f81f7de67c457e86e0a685ecad375e92aa10ff36
Danny <ilyindanny@gmail.com>
2022-07-12 02:17:24 +0300

    Update 003.py

--- files/Seminars Slavalk/seminar 002/003.py
+++ files/Seminars Slavalk/seminar 002/003.py
@@ -16,3 +16,32 @@ def f(a, b):
 
 
 print(f(a1, b2))
+
+
+n = 'Урок 1: Общий рекрутмент: процесс отбора кандидатов глазами рекрутера'
+m = 'Урок Общий рекрутментb: процессb отбора   рекрутера,'
+
+
+a = n.split(' ')
+b = m.split(' ')
+
+def f(a, b):
+    counter = 0
+    text = ''
+    for i in range(len(a)):
+        for j in range(len(b)):
+            if a[i] == b[j]:
+                a[i] = a[i].upper()
+                counter += 1
+                text += ' ' + a[i]
+        
+    
+    return a, ' всего ', counter, ' совпадения: ', text
+    
+    
+    
+        
+print(*f(a, b))     
+
+
+
