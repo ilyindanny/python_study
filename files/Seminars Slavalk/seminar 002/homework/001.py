38aeecdeba43b4ca302c2427f8bea6a7b05fa9a2
Danny <ilyindanny@gmail.com>
2022-07-12 18:51:33 +0300

    Update 001.py

--- files/Seminars Slavalk/seminar 002/homework/001.py
+++ files/Seminars Slavalk/seminar 002/homework/001.py
@@ -10,12 +10,12 @@
 #
 #
 # def sum_of_digit(n):
-# 	result = 0
-# 	for i in range(len(n)):
-# 		if not n[i].isdigit():
-# 			continue
-# 		result += float(num[i])
-# 	print(result)
+#     result = 0
+#     for i in range(len(n)):
+#         if not n[i].isdigit():
+#             continue
+#         result += float(num[i])
+#     print(result)
 #
 #
 # sum_of_digit(num)
@@ -31,10 +31,10 @@
 #
 #
 # def mult_of_digit(n):
-# 	result = 1
-# 	for i in range(1, n + 1):
-# 		result *= i
-# 	print(result)
+#     result = 1
+#     for i in range(1, n + 1):
+#         result *= i
+#     print(result)
 #
 #
 # mult_of_digit(num)
@@ -49,10 +49,10 @@
 #
 #
 # def func(n):
-# 	result = 0
-# 	for i in range(1, n + 1):
-# 		result += ((1 + 1 / i) ** i)
-# 	print(round(result, 3))
+#     result = 0
+#     for i in range(1, n + 1):
+#         result += ((1 + 1 / i) ** i)
+#     print(round(result, 3))
 #
 #
 # func(num)
@@ -64,30 +64,72 @@
 #
 #
 # def fill_list(n):
-# 	col = []
-# 	index = 1
-# 	for i in range(n):
-# 		col.append(random.randint(-n, n))
-# 		index += 1
-# 	print(col)
-# 	return col
+#     col = []
+#     index = 1
+#     for i in range(n):
+#         col.append(random.randint(-n, n))
+#         index += 1
+#     print(col)
+#     return col
 #
 #
 # def get_user_nums(n):
-# 	print('введите позиции через пробел (в диапазоне 0 до {}): '.format(n - 1), end='')
-# 	user_nums = list(map(int, input().split()))
-# 	return user_nums
+#     print('введите позиции через пробел (в диапазоне 0 до {}): '.format(n - 1), end='')
+#     user_nums = list(map(int, input().split()))
+#     return user_nums
 #
 #
 # def mult_num(col, user_nums):
-# 	result = 1
-# 	for i in user_nums:
-# 		result *= col[i]
-# 	print(result)
+#     result = 1
+#     for i in user_nums:
+#         result *= col[i]
+#     print(result)
 #
 #
 # mult_num(fill_list(num), get_user_nums(num))
 
 # Реализуйте алгоритм перемешивания списка.
 
-def
+import random
+
+
+def my_collection():
+    n = int(input('введите размер коллекции: '))
+    col = []
+    for i in range(n):
+        col.append(i)
+
+    print('исходная коллекция: {}'.format(col))
+    return col
+
+
+
+def mix_collection(col):
+    
+    # коллекция с перемешанным списком
+    new_col = []
+    
+    # список неиспользованных индексов в исходной коллекции
+    index_list = []
+    
+    # заполнить список индексами от 0 до длины col - 1
+    for i in range(len(col)):
+        index_list.append(i)
+
+    
+    # наподнение нового списка с перемешиванием
+    for i in range(len(col)):
+        
+        # рандомный индекс
+        j = random.randint(0, len(index_list) - 1)
+        
+        # добавление в новую коллекцию случайного индекса
+        new_col.append(index_list[j])
+        
+        # удаление использованного индекса из списка индексов
+        index_list.remove(index_list[j])
+    print('новая коллекция: {}'.format(new_col))
+
+
+mix_collection(my_collection())
+
