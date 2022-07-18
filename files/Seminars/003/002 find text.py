bdcd367e63e0312793c8c7d7b4dc183a9233e68a
Ilyin Danny <ilyindanny@gmail.com>
2022-07-18 18:20:01 +0300

    temp

--- files/Seminars/003/002 find text.py
+++ files/Seminars/003/002 find text.py
@@ -1,5 +1,3 @@
-
-
 # поиск слов:
 
 
@@ -8,78 +6,71 @@ text_of_book = data.read()
 data.close()
 
 
+# создание массива из текста без знаков препинания:
+
+def text_coll(start_str):
+	temp_coll = ''
+	for el in start_str:
+		if el == '–' or el == ',' or el == '.' or el == '!' or el == '?' or el == '-' or el == ':' or el == ';' or el == '\'' or el == '"' or el == '«' or el == '»':
+			continue
+		else:
+			temp_coll += el
+
+	result = list(temp_coll.lower().split())
+
+	return result
 
 
-def text_coll(start_coll):
-    coll = list(start_coll.lower().split())
-    temp = ''
-    
-    for i in range(len(coll)):
-        for j in range(len(coll[i])):
-            
-            el = coll[i][j]
-            
-            if el == ',' or el == '.' or el == '!' or el == '?' or el == '-' or el == ':' or el == ';' or el == '\'' or el == '"':
-                continue
-            else:
-                temp += el
-        coll[i] = temp
-    
-    return coll
-    
-    
-    
+# создать список с уникальными словами и указанием количества использования каждого слова:
 
-# создать список с уеикальными словами и указантем количества использования каждого слова:
+def literary_method(coll):
+	length = len(coll)
+	result = []
+	while length:
+		count = 0
+		index = 0
+		n = coll[0]
 
-def literary_method(start_col):
-    coll = list(start_col.lower().split())
-    length = len(coll)
-    result = []
-    while length:
-        count = 0
-        index = 0
-        n = coll[0]
+		while index <= length - 1:
+			if n == coll[index]:
+				del coll[index]
+				count += 1
+				length -= 1
+			else:
+				index += 1
+		result.append(count)
+		result.append(n)
 
-        while index <= length - 1:
-            if n == coll[index]:
-                del coll[index]
-                count += 1
-                length -= 1
-            else:
-                index += 1
-        result.append(count)
-        result.append(n)
-
-    return result
+	return result
 
 
-
-# сортировка по частоте испольщования:
+# сортировка по частоте использования:
 
 def sort_result(coll):
-    coll = coll[0: len(coll)]
-    result = []
-    length = len(coll)
+	coll = coll[0: len(coll)]
+	result = []
+	length = len(coll)
 
-    while length:
-        max = 0
-       
-        for i in range(0, length - 1, 2):            
-            if coll[i] > coll[max]:
-                max = i
-        result.append(coll[max])
-        result.append(coll[max + 1])
-        del coll[max + 1]
-        del coll[max]
-        length -= 2
-    for i in range(0, len(result), 2):
-        print(result[i], ' - ', result[i + 1])
-        
-    
-        
-        
-        
-        
+	while length:
+		max_index = 0
 
-sort_result(literary_method(text_coll(text_of_book)))
+		for i in range(0, length - 1, 2):
+			if coll[i] > coll[max_index]:
+				max_index = i
+		result.append(coll[max_index])
+		result.append(coll[max_index + 1])
+		del coll[max_index + 1]
+		del coll[max_index]
+		length -= 2
+	for i in range(0, len(result), 2):
+		print(result[i], ' - ', result[i + 1])
+
+
+# получить список из букв без знаков препинания:
+collection_from_book = text_coll(text_of_book)
+
+# создать список с уникальными словами и количеством использования:
+uniq_words = literary_method(collection_from_book)
+
+# отсортировать и распечатать:
+sort_result(uniq_words)
