a20faac357c0a9848e21b6243231e405d7c518e6
Danny <ilyindanny@gmail.com>
2022-07-19 13:46:14 +0300

    Add alternative homework.py

---
+++ files/Seminars Slavalk/seminar 003/alternative homework.py
@@ -0,0 +1,87 @@
+
+#  возвращает список str, проверяя, могут ли значения быть преобразованы в float:
+def get_coll():
+    digit = 1
+    coll = []
+    temp = 0
+    while digit:
+        try:
+            coll = list(input('введите список вещественных чисел через пробел: ').split())
+            #  проверка на правильный ввод
+            only_int = 0
+            for i in coll:
+                temp = float(i)
+                
+                #  проверка. чисел с дробью не должно быть меньше двух: 
+                only_int += i.isdigit()
+                1 / (only_int - len(coll))
+                1 / (only_int - len(coll) + 1)
+            digit = 0
+                
+        except:
+            print('невозможно преобразовать в float или недостаточно дробных частей для вычисления')
+  
+    return coll
+
+
+#  возвращает список str с минимальным и максимальным дробным числом (в целой части 0)
+
+def find_max(coll):
+    new_coll = []
+    maxx = 0
+    minn = 0
+    
+    #  если элемент содержит точку, он записывается в новый список (в целой части 0):
+    for i in range(len(coll)):
+        if not coll[i].isdigit():
+            new_coll.append(float('0.' + coll[i].split('.')[1]))
+    
+    #  поиск максимальной и минимальной дробной части:
+    for j in range(len(new_coll)):
+        if new_coll[j] > new_coll[maxx]:
+            maxx = j
+        if new_coll[j] < new_coll[minn]:
+            minn = j
+
+    return [new_coll[maxx], new_coll[minn]]
+
+
+
+#  поиск разности между дробными частями с обходом погрешности float
+
+def get_int(coll):
+    factor = 1
+    
+    
+    # цикл вычисляет максимальный порядок дбробного остатка:
+    for i in coll:
+        while i * factor != int(i * factor):
+            factor *= 10
+    
+
+    return (coll[0] * factor - coll[1] * factor) / factor
+    
+
+
+num_collection = get_coll()
+
+maxx_minn_collection = find_max(num_collection)
+
+print(get_int(maxx_minn_collection))
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+    
+
+
+
+
