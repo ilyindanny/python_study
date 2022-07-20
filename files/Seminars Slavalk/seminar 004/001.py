b8bade4ac20f2dbc82bc9ddb596c79f28fc50e1f
Ilyin Danny <ilyindanny@gmail.com>
2022-07-20 20:01:18 +0300

    temp

--- files/Seminars Slavalk/seminar 004/001.py
+++ files/Seminars Slavalk/seminar 004/001.py
@@ -1,15 +1,29 @@
-with open('Seminars Slavalk/seminar 004/001.txt', 'r') as file_data:
-    data = file_data.readline()
+# Seminars Slavalk/seminar 004/001.txt
+
+with open('001.txt', 'r') as file_data:
+	data = file_data.readline()
+
+print(data)
+
 
 def find_minn_maxx(coll):
-    nums = list(coll.split())
-    maxx = nums[0]
-    minn = nums[0]
-    
-    for i in nums:
-        if i > maxx: maxx = i
-        if i < minn: minn = i
-    result = str(minn) + ' ' + str(maxx)
+	nums = list(map(int, coll.split()))
+	maxx = nums[0]
+	minn = nums[0]
+	print(nums)
+	for i in nums:
+		if i > maxx:
+			maxx = i
+		if i < minn:
+			minn = i
+	return 'min = ' + str(minn) + ',  = ' + str(maxx)
+
+
+result = find_minn_maxx(data)
 
 with open('001.py', 'a') as file_data:
-    file_data.write('\n', find_minn_maxx(data))
+	file_data.write('\n# {}\n\n'.format(result))
+
+
+# min = 1,  = 345
+
