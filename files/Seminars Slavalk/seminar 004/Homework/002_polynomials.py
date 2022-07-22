89aa1bc5e1798001d036d150a09498ae6dc1a7e0
Danny <ilyindanny@gmail.com>
2022-07-23 01:19:21 +0300

    Update 001_first_rules.py, 001.py and one other file

--- files/Seminars Slavalk/seminar 004/Homework/002_polynomials.py
+++ files/Seminars Slavalk/seminar 004/Homework/002_polynomials.py
@@ -4,42 +4,65 @@ path3 = '002_polynomials_result.txt'
 
 
 with open(path1, 'r') as file:
-	a = file.readline()
+    a = file.readline()
 with open(path2, 'r') as file:
-	b = file.readline()
+    b = file.readline()
 
 
-# поиск коэффициентов
+# разбор многочлена
 
 def get_coefficients(data: str) -> list:
-	data = data.split()[0:-2]
-	if data[0] != '-':
-		data.insert(0, '+')
-
-	nums = []
-
-	for i in range(1, len(data), 2):
-		nums.append(data[i - 1])
-		for j in data[i]:
-			if j.isdigit():
-				nums[len(nums) - 1] += j
-			else:
-				break
-	for i in range(len(nums)):
-		nums[i] = int(nums[i])
-
-	return nums
+    
+    data = data.replace('+ ', '+')
+    data = data.replace('- ', '-')
+    data = data.split()
+    
+    if data[0][0] != '-':
+        data[0] = '+' + data[0]
+    
+    res = []
+    count = 1
+    
+    for i in range(0, len(data) - 2):
+        for j in data[i][1:]:
+            if j.isdigit():
+                count +=1
+            else:
+                break
+        res.append(data[i][0] + data[i][1:count])
+        if count < len(data[i]):
+            res.append(data[i][count:])
+        count = 1
+    
+    res.append(' = 0')
+        
+    return res
 
 
 # сложение
 
 def sum_of_lists(a: list, b: list) -> list:
-	res = []
-	for i in range(len(a)):
-		res.append(a[i] + b[i])
-
-	return res
-
+    data = []
+    for i in range(len(a)):
+        try:
+            if int(a[i]) + int(b[i]) > 0:
+                data.append(' + ' + str(int(a[i]) + int(b[i])))
+            else:
+                data.append(' - ' + str(abs(int(a[i]) + int(b[i]))))
+        except:
+            data.append(a[i])
+    
+    
+    if data[0][1] == '-':
+        data[0] = '-' + data[0][3:]
+    else:
+        data[0] = data[0][3:]
+    
+    res = ''
+    for i in data:
+        res += i
+    
+    return res
 
 print('многочлен a: ', a)
 print('многочлен b: ', b)
@@ -49,6 +72,6 @@ b = get_coefficients(b)
 result = str(sum_of_lists(a, b))
 
 with open(path3, 'w') as file:
-	file.write(result)
+    file.write(result)
 
 print(f'в файл {path3} сохранен результат: {result}')
