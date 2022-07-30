6b41cb449831f001acbff73a0c30a65d7ca3a5e3
Ilyin Danny <ilyindanny@gmail.com>
2022-07-30 17:43:56 +0300

    temp

--- files/iOS/guide/004 files.py
+++ files/iOS/guide/004 files.py
@@ -1,8 +1,8 @@
 # работа с файлами
 
 
-
 import json
+
 colors = ['red ', 'green ', 'blue']  # это просто список для примеров
 
 # 'a' это  дозапись, 'w' перезапись заново, а 'r' - это чтение
@@ -11,7 +11,6 @@ data.writelines(colors)  # разделителей не будет
 data.write('\nLINE\n')
 data.close()
 
-
 # второй вариант как открыть. после этого блока связь с файлом разрывается автоматически
 
 
@@ -19,25 +18,21 @@ data.close()
 # "альяс" - это замена имени файла на псевдоним (в примере это псевдоним data):
 
 with open('file.txt', 'w') as data:  # означает "воспринимать оператор как переменную data"
-    data.write('LINE = 1\n')
-
+	data.write('LINE = 1\n')
 
 # вариант открытия для файла json:
 
 
 with open('file.json', 'w', encoding='utf-8') as data:
-    data.write(json.dumps('LINE = 1\n', ensure_ascii=False))
-
+	data.write(json.dumps('LINE = 1\n', ensure_ascii=False))
 
 # третий вариант наверно
 
 path = 'file.txt'
 data = open(path, 'r')
 for line in data:
-    print('{} line + data'.format(line))
+	print('{} line + data'.format(line))
 data.close()
-
-
 
 # Для имён, содержащих пробелы, необходимо использовать двойные кавычки внутри строки.
 
@@ -47,7 +42,6 @@ source = ['"C:\\My Documents"', 'C:\\Code']
 # Если в строке адреса обратный слэш, то он воспринимается программой как знак отмены символа. Поэтому нужно использовать сырую строку:
 
 source = r'files\iOS\guide\004 files.py'
-
 
 # Пример созранения данных в файл при помощи встроенной библиотеки pickle. Сохранение в битовом формате данных. Такая запись в файл называется консервацией pickling, а извлечение данных из файла расконсервацией unpickling:
 
@@ -65,7 +59,10 @@ shoplist = ['яблоки', 'манго', 'морковь']
 
 f = open(shoplistfile, 'wb')
 
-pickle.dump(shoplist, f) # помещаем объект в файл f.close()
+pickle.dump(shoplist, f)  # помещаем объект в файл f.close()
 
-del shoplist # удаление переменной shoplist
+del shoplist  # удаление переменной shoplist
 
+# Тернарный оператор:
+
+s = lambda x: True if (x > 10) else False
